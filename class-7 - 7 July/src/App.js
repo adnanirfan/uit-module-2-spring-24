7d5013bb71ef4bd255670ed99420394dbfc7845e
Adnan Irfan <adnanshurta@gmail.com>
2024-07-07 15:05:07 +0500

    Added code for class-7 - fetch Data

---
+++ class-7 - 7 July/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import DataFetcher from "./DataFetcher";
+
+function App() {
+  return <DataFetcher />;
+}
+
+export default App;
---
+++ class-7 - 7 July/src/DataFetcher.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+function DataFetcher() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://api.sampleapis.com/coffee/hot");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // The empty array means this effect runs once after the initial render
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Data:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default DataFetcher;
