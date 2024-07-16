e63c4606d510f81b0ade342b2f10bf8bf0deb03b
Adnan Irfan <adnanshurta@gmail.com>
2024-07-16 18:36:14 +0500

    Added code for class-8 react router

---
+++ class-8 - 14 July/src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const Home = () => <h1>Home</h1>;
+const Contact = () => <h1>Contact</h1>;
+const About = () => <h1>About</h1>;
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
